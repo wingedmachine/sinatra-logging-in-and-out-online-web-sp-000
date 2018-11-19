e89896c3591a00089144f0dfbc32c439e07e7050
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-19 16:16:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    binding.pry
     user = User.find_by(username: params[:username], password: params[:password])
+    binding.pry
     sesssion[:user_id] = user.id
 
     redirect '/account'
