01a083dee5ddabe6a8efe419bafe27c2e9642f53
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-19 16:18:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,12 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     user = User.find_by(username: params[:username], password: params[:password])
-    session[:user_id] = user.id
-
-    redirect '/account'
+    if user
+      session[:user_id] = user.id
+      redirect '/account'
+    else
+      redirect '/error'
+    end
   end
 
   get '/account' do
