0fbc24f7ee476452d278d4e61d1f29cd10d934ff
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-19 16:52:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     if session[:user_id] && User.find(session[:user_id])
-      binding.pry
       erb :account
     else
       erb :error
